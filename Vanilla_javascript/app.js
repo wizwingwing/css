41c666458829c57655f9fc2be0881e8e5be32e36
wizwingwing <61519634+wizwingwing@users.noreply.github.com>
2021-03-17 18:16:35 +0700

    notcompress
    32.16
    Beginner Vanilla Javascript Project Tutorial

---
+++ Vanilla_javascript/app.js
@@ -0,0 +1,39 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+// Functions
+
+function addTodo(event)
+{
+    // Prevent form form submitting
+   event.preventDefault()
+    // DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText= "HEY";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // CHECK trash BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv);
+    //Clear Todo Input Value
+    todoInput.value = "";
+
+}
