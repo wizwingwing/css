e4d32db61f5ce748288fc4f6b7bed49580e6c67d
wizwingwing <61519634+wizwingwing@users.noreply.github.com>
2021-03-18 16:24:49 +0700

    1

--- Vanilla_javascript/app.js
+++ Vanilla_javascript/app.js
@@ -2,10 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click',filterTodo);
+
 
 // Functions
 
@@ -18,9 +21,11 @@ function addTodo(event)
     todoDiv.classList.add("todo");
     // Create LI
     const newTodo = document.createElement('li');
-    newTodo.innerText= "HEY";
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    // ADD TODO TO localStorage.POST
+    saveLocalTodos(todoInput.value);
     //CHECK MARK BUTTON
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -35,5 +40,72 @@ function addTodo(event)
     todoList.appendChild(todoDiv);
     //Clear Todo Input Value
     todoInput.value = "";
-
 }
+
+function deleteCheck(e)
+{
+    const item = e.target;
+    // Delete Todo
+    if (item.classList[0]=== "trash-btn")
+    {
+        const todo = item.parentElement;
+        //Animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+    //Check Mark
+    if(item.classList[0] === "complete-btn")
+    {
+        const todo = item.parentElement;
+        todo.classList.toggle('complete');
+    }
+}
+
+function filterTodo(e)
+{
+    const todos =todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value)
+        {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains('complete'))
+                {
+                    todo.style.display = 'flex';
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('complete'))
+                {
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+
+function saveLocalTodos(todo)
+{
+    // Check---Hey Do I already have thing in there?
+    let todos;
+    if(localStorage.getItem("todos") === null)
+    {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+
